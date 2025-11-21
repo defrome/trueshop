102c636c449cd7c36b4dc5e79e89c438133476dd
defrome <iskoblov555@gmail.com>
2025-11-21 20:44:17 +0300

    database make utils

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 from app.config import settings
 
 engine = create_engine(
@@ -20,3 +19,4 @@ def get_db():
         db.close()
 
 def init_db():
+    Base.metadata.create_all(bind=engine)
